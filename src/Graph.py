fc931213bcb83d5c0e2d2a47e4e650a8addd695e
lhc <lhcezx@gmail.com>
2021-11-07 19:47:11 +0100

    version

---
+++ src/Graph.py
@@ -0,0 +1,64 @@
+from logging import log
+import dgl
+import tensorflow as tf
+import numpy as np
+import pdb
+
+from dgl.nn.tensorflow import conv, glob, HeteroGraphConv
+from tensorflow.keras import Model, layers, Input
+
+class contextual_layers(layers.Layer):
+    def __init__(self, in_feats, h_feats, **kwarg):
+        super().__init__(**kwarg)
+        self.gat1 = conv.GATConv(in_feats, h_feats, 1)
+        self.gat2 = conv.GATConv(in_feats, h_feats, 1)
+        self.gat3= conv.GATConv(in_feats, h_feats, 1)
+
+    def call(self, g, in_feat):
+        h = self.gat1(g, in_feat)
+        h = tf.nn.relu(h)
+        h = self.gat2(g, h)
+        h = tf.nn.relu(h)
+        h = self.gat3(g, h)
+        return h
+
+class hierarchical_layers(layers.Layer):
+    def __init__(self, in_feats, h_feats, **kwarg):
+        super().__init__(**kwarg)
+        self.gat1 = conv.GATConv(in_feats, h_feats, 1)
+        self.gat2 = conv.GATConv(in_feats, h_feats, 1)
+        self.gat3= conv.GATConv(in_feats, h_feats, 1)
+
+    def call(self, g, in_feat):
+        h = self.gat1(g, in_feat)
+        h = tf.nn.relu(h)
+        h = self.gat2(g, h)
+        h = tf.nn.relu(h)
+        h = self.gat3(g, h)
+        return h
+
+if __name__ == '__main__':
+    # Graph construction
+    dim_h = 256
+    g1 = dgl.graph(([0, 0, 0, 0, 0], [1, 2, 3, 4, 5]), num_nodes = 1024)
+
+
+    g2 = dgl.graph(([0, 0, 0], [1, 2, 3]), num_nodes = 4)
+    g1 = dgl.add_reverse_edges(g1)
+    g2 = dgl.add_reverse_edges(g2)
+    # load data to graph
+    features = g1.ndata['x'] = tf.ones((g1.num_nodes(), 256))
+    features = g2.ndata['x'] = tf.ones((g2.num_nodes(), 256))
+
+
+    lay1 = contextual_layers(g1.ndata['x'].shape[1], dim_h)
+    
+    h_out = lay1(g1, features)
+    h_out = tf.squeeze(h_out)
+    # print(g1.ndata['x'].shape)
+    # print(h_out.shape)
+
+
+
+    features = g1.ndata['x'] = tf.ones((g1.num_nodes(), 256))
+    model = contextual_layers(g1.ndata['x'].shape[1], dim_h)
---
+++ src/utils.py
@@ -0,0 +1,37 @@
+from math import factorial
+from pycparser.c_ast import Continue
+import tensorflow as tf
+import numpy as np
+import dgl
+import datetime
+import pdb
+
+def stochastic_create_edges(g, n_edges = 0):
+
+    assert n_edges>g.num_nodes(), "number of edges is smaller than that of nodes"
+    max_edges = (g.num_nodes()-1) * g.num_nodes() // 2  # (0 + g.num_nodes()-1) * g.num_nodes() / 2 
+    # Ensure that every nodes has least one edge 
+    for i in range(1, g.num_nodes()-1):
+        j = np.random.randint(i+1, g.num_nodes())
+        g.add_edges(tf.constant([i], dtype = "int64"), tf.constant([j], dtype = "int64"))
+    
+    # Add the reset of edges
+    print(max_edges)
+    if n_edges:
+        while g.num_edges() < n_edges and g.num_edges() < max_edges:
+            i = np.random.randint(0, g.num_nodes())
+            j = np.random.randint(0, g.num_nodes())
+            g.add_edges(tf.constant([i], dtype = "int64"), tf.constant([j], dtype = "int64")) if not (g.has_edges_between(i,j) or g.has_edges_between(j,i) or i == j) else 0
+            # print(i,j)
+            # print(g.num_edges())
+            if g.num_edges() == max_edges:
+                break
+    print(g.num_edges())
+    return dgl.add_reverse_edges(g)
+
+if __name__ == "__main__":
+    starttime = datetime.datetime.now()
+    g1 = dgl.graph(([0], [1]), num_nodes = 4096)
+    g1 = stochastic_create_edges(g1,100000)
+    endtime = datetime.datetime.now()
+    print((endtime - starttime).seconds)
